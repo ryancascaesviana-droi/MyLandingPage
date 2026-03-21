0b309462bf567a0bc55371fa93157c1d9a99229d
ryancascaesviana-droi <ryancascaesviana@gmail.com>
2026-03-21 09:03:28 -0300

    aula

---
+++ src/script.js
@@ -0,0 +1,22 @@
+
+function click() {
+    confirm("essa mensangem foi exibida corretamente")
+    
+}
+
+document.addEventListener("click",click)
+
+var projetos = [
+    {titulo:"meu incrivel projeto 1", descricao: "meu projeto eh muito massa", link:"#"}
+    {titulo:"meu incrivel projeto 2", descrição: "meu projeto é muito massa", link:"#"}
+    {titulo:"meu incrivel projeto 3", descrição:"meu projeto é muito massa", link: "#"}
+    {titulo:"meu incrivel projeto 4", descrição "meu projeto é muito massa", lnk:"#"}
+
+  console.log
+
+
+]
+
+
+
+
