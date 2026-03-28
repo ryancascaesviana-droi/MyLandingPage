7eac0ca07edbbf4b624cae79edc38f4cc4e3c49a
ryancascaesviana-droi <ryancascaesviana@gmail.com>
2026-03-28 09:00:47 -0300

    ddvvv

--- src/script.js
+++ src/script.js
@@ -1,21 +1,31 @@
 
-function click() {
-    confirm("essa mensangem foi exibida corretamente")
-    
-}
 
-document.addEventListener("click",click)
 
 var projetos = [
-    {titulo:"meu incrivel projeto 1", descricao: "meu projeto eh muito massa", link:"#"}
-    {titulo:"meu incrivel projeto 2", descrição: "meu projeto é muito massa", link:"#"}
-    {titulo:"meu incrivel projeto 3", descrição:"meu projeto é muito massa", link: "#"}
-    {titulo:"meu incrivel projeto 4", descrição "meu projeto é muito massa", lnk:"#"}
+  { titulo: "meu incrivel projeto 1", descricao: "meu projeto eh muito massa", link: "#" },
+  { titulo: "meu incrivel projeto 2", descrição: "meu projeto é muito massa", link: "#" },
+  { titulo: "meu incrivel projeto 3", descrição: "meu projeto é muito massa", link: "#" },
+  { titulo: "meu incrivel projeto 4", descrição: "meu projeto é muito massa", lnk: "#" }
 
-  console.log
+];
+
+const projectGrid = document.querySelector('.project-grid');
+
+if (projectGrid) {
+  alert('oi')
+  projectGrid.innerHTML = '';
+  projetos.forEach(function (projeto) {
+    const card = document.createElement('a');
+    card.className = 'project-card';
+    card.href = projectGrid.link;
+    card.innerHTML = '<h3>' + projeto.titulo + '</h3><p>' + projeto.descrição + '</p>';
+    projectGrid.appendChild(card);
+    
+  });
+
+}
 
 
-]
 
 
 
