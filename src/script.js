c829cb655ebf7f153329f41d533177124cfda2d9
ryancascaesviana-droi <ryancascaesviana@gmail.com>
2026-04-11 08:56:22 -0300

    aula pré-finalização

--- src/script.js
+++ src/script.js
@@ -2,22 +2,22 @@
 
 
 var projetos = [
-  { titulo: "meu incrivel projeto 1", descricao: "meu projeto eh muito massa", link: "#" },
-  { titulo: "meu incrivel projeto 2", descrição: "meu projeto é muito massa", link: "#" },
-  { titulo: "meu incrivel projeto 3", descrição: "meu projeto é muito massa", link: "#" },
-  { titulo: "meu incrivel projeto 4", descrição: "meu projeto é muito massa", lnk: "#" }
+  { titulo: "Proejeto 1: Totem maluco ", descrição: "Projeto de programação criativa usando o p5js para desenhar um totem", link: "https://editor.p5js.org/ryancascaesviana/sketches/7yMGKtBd5" },
+  { titulo: "Proejeto 2: Emoções", descrição: "Projeto de programaçãõ de mundança de emoções com p5js", link: "https://editor.p5js.org/ryancascaesviana/sketches/2dq8e7xct" },
+  { titulo: "Proejeto 3: Contador", descrição: "projeto de programação com contador de numeros no p5js", link: "https://editor.p5js.org/ryancascaesviana/sketches/Kl53HhxS6" },
+  { titulo: "Proejeto 4: Flor", descrição: "projeto de programação criativo desenhando uma flor no p5js", link: "https://editor.p5js.org/ryancascaesviana/sketches/O-t4zRQHU" },
+  
 
 ];
 
 const projectGrid = document.querySelector('.project-grid');
 
 if (projectGrid) {
-  alert('oi')
   projectGrid.innerHTML = '';
   projetos.forEach(function (projeto) {
     const card = document.createElement('a');
     card.className = 'project-card';
-    card.href = projectGrid.link;
+    card.href = projeto.link;
     card.innerHTML = '<h3>' + projeto.titulo + '</h3><p>' + projeto.descrição + '</p>';
     projectGrid.appendChild(card);
     
